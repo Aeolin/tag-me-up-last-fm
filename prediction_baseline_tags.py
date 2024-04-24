882945979416b9f458257135dbc16083c1b177e3
Aeolin Ferjünnoz <aeolin@aeol.in>
2024-04-24 22:07:56 +0000

    - cleaned up code - ran blind baseline comparison - ran finetune comparison - start reorganising files

---
+++ prediction_baseline_tags.py
@@ -0,0 +1,77 @@
+import json
+import pandas as pd
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import PeftModel
+import transformers
+from datetime import datetime
+import requests
+from tqdm import tqdm
+import random
+import os
+from multiprocessing import Pool, set_start_method
+
+
+
+def process_song(song):
+    eval_prompt = format_prompt(song)
+    model_input = eval_tokenizer(f"<s>{eval_prompt}", return_tensors="pt").to("cuda")
+    
+    with torch.no_grad(): 
+        result = eval_tokenizer.decode(ft_model.generate(**model_input, max_new_tokens=100, repetition_penalty=1.15)[0], skip_special_tokens=True)
+        result = result.replace(eval_prompt, '').strip()
+        try:
+            song['RawPrediction'] = result 
+            tags = json.loads(result)
+            tags = [tag.strip() for tag in tags['tags']]
+            song['PredictedTags'] = tags
+        except Exception as error:
+            song['error'] = str(error)  # Store the error in the song dictionary
+    
+    return song
+
+
+if __name__ == "__main__":
+    songs = None
+    with open('evaluation_set.json', 'rt', encoding='utf8') as infile:
+        songs = json.load(infile)
+
+    set_start_method('spawn')
+    with Pool(processes=4) as pool:  # Adjust number of processes based on your system's capabilities 
+        results = list(tqdm(pool.imap(process_song, songs), total=len(songs)))
+        with open('evaluation_results/baseline-tags/predictions.json', 'wt', encoding='utf8') as outfile:
+            json.dump(results, outfile, indent=2)
+else:
+    os.environ['HF_TOKEN'] = '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'
+
+    project = "tag-me-up-daddy-3"
+    base_model_name = "mistral"
+    run_name = base_model_name + "-" + project
+    output_dir = "./" + run_name
+
+
+    base_model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_id,  # Mistral, same as before
+        quantization_config=bnb_config,  # Same quantization config as before
+        device_map="auto",
+        trust_remote_code=True,
+        attn_implementation="flash_attention_2"
+    )
+
+    eval_tokenizer = AutoTokenizer.from_pretrained(base_model_id, add_bos_token=True, trust_remote_code=True)
+    ft_model = base_model#PeftModel.from_pretrained(base_model, "mistral-tag-me-up-daddy-3/checkpoint-500")
+
+    curated_tags = json.load(open('curated_tags.json'))
+    def format_prompt(song):
+        return f'[INST]<<SYS>>Tag the song based on the lyrics, only respond in json {{"tags": []}}\nlegal tags: [{curated_tags}]<</SYS>>\n[{song["PrimaryArtistName"]}]\n{song["Lyrics"]}[/INST]'
+
+    ft_model.eval()
---
+++ prediction_finetune.py
@@ -0,0 +1,69 @@
+import json
+import pandas as pd
+import numpy as np
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+from peft import PeftModel
+import transformers
+from datetime import datetime
+import requests
+from tqdm import tqdm
+import random
+
+project = "tag-me-up-daddy-3"
+base_model_name = "mistral"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+base_model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+)
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_id,  # Mistral, same as before
+    quantization_config=bnb_config,  # Same quantization config as before
+    device_map="auto",
+    trust_remote_code=True,
+)
+
+eval_tokenizer = AutoTokenizer.from_pretrained(base_model_id, add_bos_token=True, trust_remote_code=True)
+ft_model = PeftModel.from_pretrained(base_model, "mistral-tag-me-up-daddy-3/checkpoint-500")
+
+def format_prompt(song):
+    return f'[INST]<<SYS>>Tag the song based on the lyrics, only respond in json {{"tags": []}}<</SYS>>\n[{song["PrimaryArtistName"]}]\n{song["Lyrics"]}[/INST]'
+
+songs = None
+with open('evaluation_data/all_songs.json', 'rt', encoding='utf8') as infile:
+    songs = json.load(infile)
+    songs = list(filter(lambda x: x['Lyrics'], songs))
+
+def get_random():
+    song = random.choice(songs)
+    songs.remove(song)
+    return song
+
+random.seed(42)
+random_selection = [get_random() for x in range(8000)]
+ft_model.eval()
+
+for song in tqdm(random_selection, total=len(random_selection)):
+    eval_prompt = format_prompt(song)
+    model_input = eval_tokenizer(f"<s>{eval_prompt}", return_tensors="pt").to("cuda")
+
+    with torch.no_grad(): 
+        result = eval_tokenizer.decode(ft_model.generate(**model_input, max_new_tokens=100, repetition_penalty=1.15)[0], skip_special_tokens=True)
+        result = result.replace(eval_prompt, '').strip()
+        song['RawPrediction'] = result
+        try:
+            tags = json.loads(result)
+            song['PredictedTags'] = tags['tags']
+        except Exception as error:
+            print(error)
+
+with open('evaluation_results/finetuned/predictions.json', 'wt', encoding='utf8') as outfile:
+    json.dump(random_selection, outfile, indent=2)
